c69c95025d03be759494092321e74006a2800bcf
Giacomo Tommaso Petrucci <giacomo.tom.petrucci@gmail.com>
2025-03-22 22:56:18 +0100

    The calendar is re-rendered automatically upon setting change

--- SettingsTab.ts
+++ SettingsTab.ts
@@ -25,6 +25,17 @@ export class ExampleSettingTab extends PluginSettingTab {
 					.onChange(async (value) => {
 						this.plugin.settings.startOfWeek = value as 'Monday' | 'Sunday';
 						await this.plugin.saveSettings();
+						// Re-render the calendar
+						const activeLeaf = this.app.workspace.activeLeaf;
+						if (activeLeaf) {
+							const view = activeLeaf.view;
+							if (view.getViewType() === 'markdown') {
+								const container = view.containerEl.querySelector('.weekly-calendar-wrapper');
+								if (container) {
+									this.plugin.renderCalendar(container, view.file.path);
+								}
+							}
+						}
 					})
 			);
 	}
